b773db67bbbb9abbc6ecef0c2def5e322e0531b3
anggerwicaksono <77134048+anggerwicaksono@users.noreply.github.com>
2021-01-14 18:46:16 +0700

    Delete setup.sh

--- setup.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p ~/.streamlit/
-echo "[general]
-email = \"anggerwicaksono@student.ittelkom-sby.ac.id\"
-" > ~/.streamlit/credentials.toml
-echo "[server]
-headless = true
-port = $PORT
-enableCORS = false
-" > ~/.streamlit/config.toml
